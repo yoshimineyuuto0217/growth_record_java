f1279428c73c25710d0aad131352c1a74d97286c
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-01-26 22:22:01 +0900

    add：エラーメッセージを追加

--- src/main/java/com/jobs/growth_record_java/constant/ErrorMessage.java
+++ src/main/java/com/jobs/growth_record_java/constant/ErrorMessage.java
@@ -2,6 +2,7 @@ package com.jobs.growth_record_java.constant;
 
 public enum ErrorMessage {
     // 認証関係
+    NAME_REQUIRED("名前は必須です"),
     EMAIL_REQUIRED("メールは必須です"),
     EMAIL_INVALID("無効なメールアドレスです"),
     EMAIL_ALREADY_EXISTS("すでに登録されているメールアドレスです"),
