760594fbaa1d26d8741ae28c3de019f630a6105b
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-02-06 08:44:48 +0900

    add:token用の型をせっt

--- src/main/java/com/jobs/growth_record_java/auth/dto/LoginResponse.java
+++ src/main/java/com/jobs/growth_record_java/auth/dto/LoginResponse.java
@@ -3,12 +3,18 @@ package com.jobs.growth_record_java.auth.dto;
 public class LoginResponse {
     
     private final String message;
+    private final String token;
 
-    public LoginResponse(String message){
+    public LoginResponse(String message,String token){
         this.message = message;
+        this.token = token;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public String getToken() {
+        return token;
     }
 }
--- src/main/java/com/jobs/growth_record_java/auth/dto/RegisterResponse.java
+++ src/main/java/com/jobs/growth_record_java/auth/dto/RegisterResponse.java
@@ -3,10 +3,17 @@ package com.jobs.growth_record_java.auth.dto;
 public class RegisterResponse {
 
     private final String message;
+    private final String token;
 
-    public RegisterResponse(String message) {
+    public RegisterResponse(String message,String token) {
+        this.token = token;
         this.message = message;
     }
+
+    public String getToken() {
+        return token;
+    }
+
 
     public String getMessage() {
         return message;
