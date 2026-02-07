ad0aea864787f34483d5c62d76931ec748206a28
吉嶺勇斗 <jilingyongdou@gmail.com>
2026-02-07 22:23:56 +0900

    refactor：名前が不適切だったので修正

--- src/main/java/com/jobs/growth_record_java/auth/repository/AuthRepository.java
+++ src/main/java/com/jobs/growth_record_java/auth/repository/AuthRepository.java
@@ -5,12 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.jobs.growth_record_java.auth.domain.model.User;
+import com.jobs.growth_record_java.domain.model.User;
 
 
 @Repository
 // JpaRepositoryはsave・updateなどを使用できるDI
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface AuthRepository extends JpaRepository<User, Long> {
     // 処理の中身を書かなくても走る理由は裏側でSQLを作成してくれてる
     // 注意点は名前の付け方
     // 動詞 + By + 条件で命名することが大事
